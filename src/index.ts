5b730f2084cbc68df4fdc10b931dc620946cb7ea
Simon Marchi <simon.marchi@ericsson.com>
2018-11-14 15:49:45 -0500

    Make module importable, add getExecPath function
    The getExecPath returns the absolute path to the debug adapter entry
    point.  The idea is that a javascript/typescript application can depend
    on the cdt-gdb-adapter module, and call getExecPath to know which file
    to spawn in order to start the adapter.
    
    Signed-off-by: Simon Marchi <simon.marchi@ericsson.com>

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/*********************************************************************
+ * Copyright (c) 2018 Ericsson and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+
+import * as path from 'path';
+
+/**
+ * Return the path to the debug adapter entry point.
+ */
+export function getExecPath(): string {
+    return path.join(__dirname, 'debugAdapter.js');
+}
