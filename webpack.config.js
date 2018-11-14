fee803e47b05c3ee580946a629cba863a7a45633
Doug Schaefer <dschaefer@blackberry.com>
2018-11-14 10:31:23 -0500

    Make the webpack an array for downstream.
    This allows us to add multiple configs if needed
    in the future without impacting downstream consumers
    like cdg-gdb-vscode.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
 const path = require('path');
 
-module.exports = {
-    target: 'node',
-    mode: 'none',
-    context: __dirname,
-    resolve: {
-        extensions: [ '.ts', '.js' ]
-    },
-    entry: {
-        debugAdapter: './src/debugAdapter.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, 'out'),
-        filename: '[name].js',
-        libraryTarget: 'commonjs',
-        devtoolModuleFilenameTemplate: '[absolute-resource-path]'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                options: {
-                    compilerOptions: {
-                        sourceMap: true
+module.exports = [
+    {
+        target: 'node',
+        mode: 'none',
+        context: __dirname,
+        resolve: {
+            extensions: [ '.ts', '.js' ]
+        },
+        entry: {
+            debugAdapter: './src/debugAdapter.ts'
+        },
+        output: {
+            path: path.resolve(__dirname, 'out'),
+            filename: '[name].js',
+            libraryTarget: 'commonjs',
+            devtoolModuleFilenameTemplate: '[absolute-resource-path]'
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        compilerOptions: {
+                            sourceMap: true
+                        }
                     }
                 }
-            }
-        ]
+            ]
+        }
     }
-};
+];
