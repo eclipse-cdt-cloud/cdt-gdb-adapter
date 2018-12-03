07dc28407165749a04613b5e97750188702087f4
Jonathan Williams <jonwilliams@blackberry.com>
2018-12-03 14:15:15 -0500

    Another pass with the formatter.
    Change-Id: I6ed39d2ade66ce1f706261edbf6323b81cac65d4
    Signed-off-by: Jonathan Williams <jonwilliams@blackberry.com>

--- src/integration-tests/var.spec.ts
+++ src/integration-tests/var.spec.ts
@@ -25,70 +25,70 @@ const testProgramsDir = path.join(__dirname, '..', '..', 'src', 'integration-tes
 const varsProgram = path.join(testProgramsDir, 'vars');
 const varsSrc = path.join(testProgramsDir, 'vars.c');
 
-beforeEach(async function() {
-  // Build the test program
-  cp.execSync('make', { cwd: testProgramsDir });
+beforeEach(async function () {
+    // Build the test program
+    cp.execSync('make', { cwd: testProgramsDir });
 
-  let args: string = getExecPath();
-  if (process.env.INSPECT_DEBUG_ADAPTER) {
-    args = '--inspect-brk ' + args;
-  }
+    let args: string = getExecPath();
+    if (process.env.INSPECT_DEBUG_ADAPTER) {
+        args = '--inspect-brk ' + args;
+    }
 
-  dc = new DebugClient('node', args, 'cppdbg', {
-    shell: true,
-  });
-  await dc.start();
-  await dc.initializeRequest();
-  await dc.hitBreakpoint({ verbose: true, program: varsProgram }, { path: varsSrc, line: 5 });
-  threads = await dc.threadsRequest();
-  expect(threads.body.threads.length).to.equal(1);
-  const stack = await dc.stackTraceRequest({ threadId: threads.body.threads[0].id });
-  expect(stack.body.stackFrames.length).to.equal(1);
-  scopes = await dc.scopesRequest({ frameId: stack.body.stackFrames[0].id });
-  expect(scopes.body.scopes.length).to.equal(1);
+    dc = new DebugClient('node', args, 'cppdbg', {
+        shell: true,
+    });
+    await dc.start();
+    await dc.initializeRequest();
+    await dc.hitBreakpoint({ verbose: true, program: varsProgram }, { path: varsSrc, line: 5 });
+    threads = await dc.threadsRequest();
+    expect(threads.body.threads.length).to.equal(1);
+    const stack = await dc.stackTraceRequest({ threadId: threads.body.threads[0].id });
+    expect(stack.body.stackFrames.length).to.equal(1);
+    scopes = await dc.scopesRequest({ frameId: stack.body.stackFrames[0].id });
+    expect(scopes.body.scopes.length).to.equal(1);
 });
 
-afterEach(async function() {
-  await dc.stop();
+afterEach(async function () {
+    await dc.stop();
 });
 
-describe('Variables Test Suite', function() {
-  // Move the timeout out of the way if the adapter is going to be debugged.
-  if (process.env.INSPECT_DEBUG_ADAPTER) {
-    this.timeout(9999999);
-  }
-  it('can read variables from a program', async function() {
-    const vars = await dc.variablesRequest({ variablesReference: scopes.body.scopes[0].variablesReference });
-    expect(vars.body.variables.length).to.equal(3);
-    expect(vars.body.variables[0].name).to.equal('a');
-    expect(vars.body.variables[0].value).to.equal('1');
-    expect(vars.body.variables[1].name).to.equal('b');
-    expect(vars.body.variables[1].value).to.equal('2');
-  });
+describe('Variables Test Suite', function () {
+    // Move the timeout out of the way if the adapter is going to be debugged.
+    if (process.env.INSPECT_DEBUG_ADAPTER) {
+        this.timeout(9999999);
+    }
+    it('can read variables from a program', async function () {
+        const vars = await dc.variablesRequest({ variablesReference: scopes.body.scopes[0].variablesReference });
+        expect(vars.body.variables.length).to.equal(3);
+        expect(vars.body.variables[0].name).to.equal('a');
+        expect(vars.body.variables[0].value).to.equal('1');
+        expect(vars.body.variables[1].name).to.equal('b');
+        expect(vars.body.variables[1].value).to.equal('2');
+    });
 
-  it('can set variables in a program', async function() {
-    const vr = scopes.body.scopes[0].variablesReference;
-    let vars = await dc.variablesRequest({ variablesReference: vr });
-    expect(vars.body.variables.length).to.equal(3);
-    expect(vars.body.variables[0].name).to.equal('a');
-    expect(vars.body.variables[0].value).to.equal('1');
-    expect(vars.body.variables[1].name).to.equal('b');
-    expect(vars.body.variables[1].value).to.equal('2');
-    // set the variables to something different
-    await dc.setVariableRequest({ name: 'a', value: '25', variablesReference: vr });
-    await dc.setVariableRequest({ name: 'b', value: '10', variablesReference: vr });
-    // assert that the variables have been updated to the new values
-    vars = await dc.variablesRequest({ variablesReference: vr });
-    expect(vars.body.variables.length).to.equal(3);
-    expect(vars.body.variables[0].name).to.equal('a');
-    expect(vars.body.variables[0].value).to.equal('25');
-    expect(vars.body.variables[1].name).to.equal('b');
-    expect(vars.body.variables[1].value).to.equal('10');
-    // step the program and see that the values were passed to the program and evaluated.
-    await dc.nextRequest({ threadId: threads.body.threads[0].id });
-    vars = await dc.variablesRequest({ variablesReference: vr });
-    expect(vars.body.variables.length).to.equal(3);
-    expect(vars.body.variables[2].name).to.equal('c');
-    expect(vars.body.variables[2].value).to.equal('35');
-  });
+    it('can set variables in a program', async function () {
+        const vr = scopes.body.scopes[0].variablesReference;
+        let vars = await dc.variablesRequest({ variablesReference: vr });
+        expect(vars.body.variables.length).to.equal(3);
+        expect(vars.body.variables[0].name).to.equal('a');
+        expect(vars.body.variables[0].value).to.equal('1');
+        expect(vars.body.variables[1].name).to.equal('b');
+        expect(vars.body.variables[1].value).to.equal('2');
+        // set the variables to something different
+        await dc.setVariableRequest({ name: 'a', value: '25', variablesReference: vr });
+        await dc.setVariableRequest({ name: 'b', value: '10', variablesReference: vr });
+        // assert that the variables have been updated to the new values
+        vars = await dc.variablesRequest({ variablesReference: vr });
+        expect(vars.body.variables.length).to.equal(3);
+        expect(vars.body.variables[0].name).to.equal('a');
+        expect(vars.body.variables[0].value).to.equal('25');
+        expect(vars.body.variables[1].name).to.equal('b');
+        expect(vars.body.variables[1].value).to.equal('10');
+        // step the program and see that the values were passed to the program and evaluated.
+        await dc.nextRequest({ threadId: threads.body.threads[0].id });
+        vars = await dc.variablesRequest({ variablesReference: vr });
+        expect(vars.body.variables.length).to.equal(3);
+        expect(vars.body.variables[2].name).to.equal('c');
+        expect(vars.body.variables[2].value).to.equal('35');
+    });
 });
