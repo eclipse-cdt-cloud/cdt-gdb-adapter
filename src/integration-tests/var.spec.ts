205a78568b72a030b161b1c8988a0148201c40e6
Simon Marchi <simon.marchi@ericsson.com>
2019-01-16 11:49:53 -0500

    Factor out some common test code
    - Move the initialization code to a common function (standardBeforeEach)
    - Move building the test programs to a common function, called only once
      per test file (standardBefore)
    - Move testProgramsDir to a common location as well, since it is always
      the same
    
    Signed-off-by: Simon Marchi <simon.marchi@ericsson.com>

--- src/integration-tests/var.spec.ts
+++ src/integration-tests/var.spec.ts
@@ -9,37 +9,26 @@
  *********************************************************************/
 
 import { expect } from 'chai';
-import * as cp from 'child_process';
 import * as path from 'path';
 import { DebugClient } from 'vscode-debugadapter-testsupport';
-import * as utils from './utils';
+import { getScopes, Scope, standardBefore, standardBeforeEach, testProgramsDir, verifyVariable } from './utils';
 
 // Allow non-arrow functions: https://mochajs.org/#arrow-functions
 // tslint:disable:only-arrow-functions
 
 let dc: DebugClient;
-let scope: utils.Scope;
-const testProgramsDir = path.join(__dirname, '..', '..', 'src', 'integration-tests', 'test-programs');
+let scope: Scope;
 const varsProgram = path.join(testProgramsDir, 'vars');
 const varsSrc = path.join(testProgramsDir, 'vars.c');
 const numVars = 8; // number of variables in the main() scope of vars.c
 
+before(standardBefore);
+
 beforeEach(async function() {
-    // Build the test program
-    cp.execSync('make', { cwd: testProgramsDir });
+    dc = await standardBeforeEach();
 
-    let args: string = path.join(__dirname, '..', 'debugAdapter.js');
-    if (process.env.INSPECT_DEBUG_ADAPTER) {
-        args = '--inspect-brk ' + args;
-    }
-
-    dc = new DebugClient('node', args, 'gdb', {
-        shell: true,
-    });
-    await dc.start();
-    await dc.initializeRequest();
     await dc.hitBreakpoint({ verbose: true, program: varsProgram }, { path: varsSrc, line: 19 });
-    scope = await utils.getScopes(dc);
+    scope = await getScopes(dc);
     expect(scope.scopes.body.scopes.length, 'Unexpected number of scopes returned').to.equal(1);
 });
 
@@ -57,43 +46,43 @@ describe('Variables Test Suite', function() {
         const vr = scope.scopes.body.scopes[0].variablesReference;
         let vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[0], 'a', 'int', '1');
-        utils.verifyVariable(vars.body.variables[1], 'b', 'int', '2');
+        verifyVariable(vars.body.variables[0], 'a', 'int', '1');
+        verifyVariable(vars.body.variables[1], 'b', 'int', '2');
         // set the variables to something different
         await dc.setVariableRequest({ name: 'a', value: '25', variablesReference: vr });
         await dc.setVariableRequest({ name: 'b', value: '10', variablesReference: vr });
         // assert that the variables have been updated to the new values
         vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[0], 'a', 'int', '25');
-        utils.verifyVariable(vars.body.variables[1], 'b', 'int', '10');
+        verifyVariable(vars.body.variables[0], 'a', 'int', '25');
+        verifyVariable(vars.body.variables[1], 'b', 'int', '10');
         // step the program and see that the values were passed to the program and evaluated.
         await dc.nextRequest({ threadId: scope.threadId });
         vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[2], 'c', 'int', '35');
+        verifyVariable(vars.body.variables[2], 'c', 'int', '35');
     });
 
     it('can read and set struct variables in a program', async function() {
         // step past the initialization for the structure
         await dc.nextRequest({ threadId: scope.threadId });
         await dc.nextRequest({ threadId: scope.threadId });
-        scope = await utils.getScopes(dc);
+        scope = await getScopes(dc);
         expect(scope.scopes.body.scopes.length, 'Unexpected number of scopes returned').to.equal(1);
         // assert we can see the struct and its elements
         let vr = scope.scopes.body.scopes[0].variablesReference;
         let vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[3], 'r', 'struct foo', '{...}', true);
+        verifyVariable(vars.body.variables[3], 'r', 'struct foo', '{...}', true);
         const childVR = vars.body.variables[3].variablesReference;
         let children = await dc.variablesRequest({ variablesReference: childVR });
         expect(
             children.body.variables.length,
             'There is a different number of child variables than expected',
         ).to.equal(3);
-        utils.verifyVariable(children.body.variables[0], 'x', 'int', '1');
-        utils.verifyVariable(children.body.variables[1], 'y', 'int', '2');
-        utils.verifyVariable(children.body.variables[2], 'z', 'struct bar', '{...}', true);
+        verifyVariable(children.body.variables[0], 'x', 'int', '1');
+        verifyVariable(children.body.variables[1], 'y', 'int', '2');
+        verifyVariable(children.body.variables[2], 'z', 'struct bar', '{...}', true);
         // set the variables to something different
         await dc.setVariableRequest({ name: 'x', value: '25', variablesReference: childVR });
         await dc.setVariableRequest({ name: 'y', value: '10', variablesReference: childVR });
@@ -103,36 +92,36 @@ describe('Variables Test Suite', function() {
             children.body.variables.length,
             'There is a different number of child variables than expected',
         ).to.equal(3);
-        utils.verifyVariable(children.body.variables[0], 'x', 'int', '25');
-        utils.verifyVariable(children.body.variables[1], 'y', 'int', '10');
+        verifyVariable(children.body.variables[0], 'x', 'int', '25');
+        verifyVariable(children.body.variables[1], 'y', 'int', '10');
         // step the program and see that the values were passed to the program and evaluated.
         await dc.nextRequest({ threadId: scope.threadId });
-        scope = await utils.getScopes(dc);
+        scope = await getScopes(dc);
         expect(scope.scopes.body.scopes.length, 'Unexpected number of scopes returned').to.equal(1);
         vr = scope.scopes.body.scopes[0].variablesReference;
         vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[4], 'd', 'int', '35');
+        verifyVariable(vars.body.variables[4], 'd', 'int', '35');
     });
 
     it('can read and set nested struct variables in a program', async function() {
         // step past the initialization for the structure
         await dc.nextRequest({ threadId: scope.threadId });
         await dc.nextRequest({ threadId: scope.threadId });
-        scope = await utils.getScopes(dc);
+        scope = await getScopes(dc);
         expect(scope.scopes.body.scopes.length, 'Unexpected number of scopes returned').to.equal(1);
         // assert we can see the 'foo' struct and its child 'bar' struct
         let vr = scope.scopes.body.scopes[0].variablesReference;
         let vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[3], 'r', 'struct foo', '{...}', true);
+        verifyVariable(vars.body.variables[3], 'r', 'struct foo', '{...}', true);
         const childVR = vars.body.variables[3].variablesReference;
         const children = await dc.variablesRequest({ variablesReference: childVR });
         expect(
             children.body.variables.length,
             'There is a different number of child variables than expected',
         ).to.equal(3);
-        utils.verifyVariable(children.body.variables[2], 'z', 'struct bar', '{...}', true);
+        verifyVariable(children.body.variables[2], 'z', 'struct bar', '{...}', true);
         // assert we can see the elements of z
         const subChildVR = children.body.variables[2].variablesReference;
         let subChildren = await dc.variablesRequest({ variablesReference: subChildVR });
@@ -140,8 +129,8 @@ describe('Variables Test Suite', function() {
             subChildren.body.variables.length,
             'There is a different number of grandchild variables than expected',
         ).to.equal(2);
-        utils.verifyVariable(subChildren.body.variables[0], 'a', 'int', '3');
-        utils.verifyVariable(subChildren.body.variables[1], 'b', 'int', '4');
+        verifyVariable(subChildren.body.variables[0], 'a', 'int', '3');
+        verifyVariable(subChildren.body.variables[1], 'b', 'int', '4');
         // set the variables to something different
         await dc.setVariableRequest({ name: 'a', value: '25', variablesReference: subChildVR });
         await dc.setVariableRequest({ name: 'b', value: '10', variablesReference: subChildVR });
@@ -151,17 +140,17 @@ describe('Variables Test Suite', function() {
             subChildren.body.variables.length,
             'There is a different number of grandchild variables than expected',
         ).to.equal(2);
-        utils.verifyVariable(subChildren.body.variables[0], 'a', 'int', '25');
-        utils.verifyVariable(subChildren.body.variables[1], 'b', 'int', '10');
+        verifyVariable(subChildren.body.variables[0], 'a', 'int', '25');
+        verifyVariable(subChildren.body.variables[1], 'b', 'int', '10');
         // step the program and see that the values were passed to the program and evaluated.
         await dc.nextRequest({ threadId: scope.threadId });
         await dc.nextRequest({ threadId: scope.threadId });
-        scope = await utils.getScopes(dc);
+        scope = await getScopes(dc);
         expect(scope.scopes.body.scopes.length, 'Unexpected number of scopes returned').to.equal(1);
         vr = scope.scopes.body.scopes[0].variablesReference;
         vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[5], 'e', 'int', '35');
+        verifyVariable(vars.body.variables[5], 'e', 'int', '35');
     });
 
     it('can read and set array elements in a program', async function() {
@@ -169,22 +158,22 @@ describe('Variables Test Suite', function() {
         const br = await dc.setBreakpointsRequest({ source: { path: varsSrc }, breakpoints: [{ line: 24 }] });
         expect(br.success).to.equal(true);
         await dc.continueRequest({ threadId: scope.threadId });
-        scope = await utils.getScopes(dc);
+        scope = await getScopes(dc);
         expect(scope.scopes.body.scopes.length, 'Unexpected number of scopes returned').to.equal(1);
         // assert we can see the array and its elements
         let vr = scope.scopes.body.scopes[0].variablesReference;
         let vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[6], 'f', 'int [3]', '[3]', true);
+        verifyVariable(vars.body.variables[6], 'f', 'int [3]', '[3]', true);
         const childVR = vars.body.variables[6].variablesReference;
         let children = await dc.variablesRequest({ variablesReference: childVR });
         expect(
             children.body.variables.length,
             'There is a different number of child variables than expected',
         ).to.equal(3);
-        utils.verifyVariable(children.body.variables[0], '[0]', 'int', '1');
-        utils.verifyVariable(children.body.variables[1], '[1]', 'int', '2');
-        utils.verifyVariable(children.body.variables[2], '[2]', 'int', '3');
+        verifyVariable(children.body.variables[0], '[0]', 'int', '1');
+        verifyVariable(children.body.variables[1], '[1]', 'int', '2');
+        verifyVariable(children.body.variables[2], '[2]', 'int', '3');
         // set the variables to something different
         await dc.setVariableRequest({ name: '[0]', value: '11', variablesReference: childVR });
         await dc.setVariableRequest({ name: '[1]', value: '22', variablesReference: childVR });
@@ -195,16 +184,16 @@ describe('Variables Test Suite', function() {
             children.body.variables.length,
             'There is a different number of child variables than expected',
         ).to.equal(3);
-        utils.verifyVariable(children.body.variables[0], '[0]', 'int', '11');
-        utils.verifyVariable(children.body.variables[1], '[1]', 'int', '22');
-        utils.verifyVariable(children.body.variables[2], '[2]', 'int', '33');
+        verifyVariable(children.body.variables[0], '[0]', 'int', '11');
+        verifyVariable(children.body.variables[1], '[1]', 'int', '22');
+        verifyVariable(children.body.variables[2], '[2]', 'int', '33');
         // step the program and see that the values were passed to the program and evaluated.
         await dc.nextRequest({ threadId: scope.threadId });
-        scope = await utils.getScopes(dc);
+        scope = await getScopes(dc);
         expect(scope.scopes.body.scopes.length, 'Unexpected number of scopes returned').to.equal(1);
         vr = scope.scopes.body.scopes[0].variablesReference;
         vars = await dc.variablesRequest({ variablesReference: vr });
         expect(vars.body.variables.length, 'There is a different number of variables than expected').to.equal(numVars);
-        utils.verifyVariable(vars.body.variables[7], 'g', 'int', '66');
+        verifyVariable(vars.body.variables[7], 'g', 'int', '66');
     });
 });
