658fb6c719f37ea9ca770da3c29d13f33d5524b9
Jonathan Williams <jonwilliams@blackberry.com>
2019-02-26 17:21:15 -0500

    Add -gdb-show functionality, and unit tests for -gdb-set and -gdb-show
    Change-Id: I895ccf710b9d7613ff2994a50bc4ef3bcb0e5fd1
    Signed-off-by: Jonathan Williams <jonwilliams@blackberry.com>

---
+++ src/integration-tests/GDBBackend.spec.ts
@@ -0,0 +1,45 @@
+/*********************************************************************
+ * Copyright (c) 2019 QNX Software Systems and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+
+import { expect } from 'chai';
+import { LaunchRequestArguments } from '..';
+import { GDBBackend } from '..';
+
+let gdb: GDBBackend;
+
+beforeEach(function() {
+    gdb = new GDBBackend();
+    const args: LaunchRequestArguments = {
+        program: 'foo',
+    };
+    gdb.spawn(args);
+});
+
+afterEach(function() {
+    gdb.sendGDBExit();
+});
+
+describe('GDB Backend Test Suite', function() {
+    // Move the timeout out of the way if the adapter is going to be debugged.
+    if (process.env.INSPECT_DEBUG_ADAPTER) {
+        this.timeout(9999999);
+    }
+
+    it('can read a value from -gdb-show', async function() {
+        const response = await gdb.sendGDBShow('width');
+        expect(response.value).to.equal('80');
+    });
+
+    it('can set a value using -gdb-set', async function() {
+        await gdb.sendGDBSet('width 88');
+        const response = await gdb.sendGDBShow('width');
+        expect(response.value).to.equal('88');
+    })
+});
