104b1fcf7ce055ba7f1d6ac961a7ee2e922858e6
Jonah Graham <jonah@kichwacoders.com>
2022-01-28 12:07:45 -0500

    Make tests resilent across gcc/gdb versions
    Different combinations of gdb/gcc consider a function breakpoint
    on different lines.
    
    Older versions stop on the first line of code, newer versions
    stop on the opening brace.
    
    This change makes the opening brace and first statement the same
    line of code so tests can be agnostic to this difference.

--- src/integration-tests/test-programs/functions.c
+++ src/integration-tests/test-programs/functions.c
@@ -1,17 +1,17 @@
 extern int other(void);
-static int staticfunc1(void) {
-    return 2;
+static int staticfunc1(void) 
+{   return 2; // make the line of code the same as opening brace to account for different gdb/gcc combinations
 }
-static int staticfunc2(void) {
-    return 2;
-}
-
-int sub(void) {
-    return 0;
+static int staticfunc2(void) 
+{   return 2; // make the line of code the same as opening brace to account for different gdb/gcc combinations
 }
 
-int main(void) {
-    staticfunc1();
+int sub(void) 
+{   return 0; // make the line of code the same as opening brace to account for different gdb/gcc combinations
+}
+
+int main(void) 
+{   staticfunc1(); // make the line of code the same as opening brace to account for different gdb/gcc combinations
     staticfunc2();
     sub();
     other();
--- src/integration-tests/test-programs/functions_other.c
+++ src/integration-tests/test-programs/functions_other.c
@@ -1,12 +1,12 @@
-static int staticfunc1(void) {
-    return 2;
+static int staticfunc1(void) 
+{   return 2; // make the line of code the same as opening brace to account for different gdb/gcc combinations
 }
-static int staticfunc2(void) {
-    return 2;
+static int staticfunc2(void) 
+{   return 2; // make the line of code the same as opening brace to account for different gdb/gcc combinations
 }
 
-int other(void) {
-    staticfunc1();
+int other(void) 
+{   staticfunc1(); // make the line of code the same as opening brace to account for different gdb/gcc combinations
     staticfunc2();
     return 0;
 }
