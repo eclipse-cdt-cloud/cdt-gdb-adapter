eclipse-cdt-cloud/cdt-gdb-adapter

commit 1893eb89150d34977ad9eaf0e806b22f3d66f0cd

Prepare for vendor specific GDB's.
The QNX gdb has a couple of different commands including the upload
command, so removing that from the base here.

Added an API to allow vendors to override the backend object.

src/GDBDebugSession.ts
@@ -16,7 +16,7 @@ import {
 import { DebugProtocol } from 'vscode-debugprotocol/lib/debugProtocol';
 import { GDBBackend } from './GDBBackend';
 import { sendBreakDelete, sendBreakInsert, sendBreakList } from './mi/breakpoint';
-import * as exec from './mi/exec';
+import { sendExecArguments, sendExecContinue, sendExecRun, sendExecNext, sendExecStep, sendExecFinish } from './mi/exec';
 import { sendStackInfoDepth, sendStackListFramesRequest, sendStackListVariables } from './mi/stack';
 import { sendTargetAttachRequest } from './mi/target';
 import { sendThreadInfoRequest } from './mi/thread';
@@ -27,7 +27,6 @@ export interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArgum
     gdb?: string;
     program: string;
     arguments?: string;
-    target: string;
     verbose?: boolean;
     logFile?: string;
 }
@@ -59,14 +58,19 @@ export interface ObjectVariableReference {
 export type VariableReference = FrameVariableReference | ObjectVariableReference;
 
 export class GDBDebugSession extends LoggingDebugSession {
-    private gdb: GDBBackend = new GDBBackend();
-    private isAttach = false;
-    private isRunning = false;
+    protected gdb: GDBBackend = this.createBackend();
+    protected isAttach = false;
+    protected isRunning = false;
+
     private frameHandles = new Handles<FrameReference>();
     private variableHandles = new Handles<VariableReference>();
 
     constructor() {
         super();
+    }
+
+    protected createBackend(): GDBBackend {
+        return new GDBBackend();
     }
 
     protected initializeRequest(response: DebugProtocol.InitializeResponse,
@@ -82,15 +86,14 @@ export class GDBDebugSession extends LoggingDebugSession {
         try {
             logger.setup(args.verbose ? Logger.LogLevel.Verbose : Logger.LogLevel.Warn, args.logFile || false);
 
-            this.isAttach = true;
-
             this.gdb.on('consoleStreamOutput', (output, category) => {
                 this.sendEvent(new OutputEvent(output, category));
             });
 
             this.gdb.on('async', (result) => this.handleGDBAsync(result));
 
-            await this.gdb.attach(args);
+            await this.gdb.spawn(args);
+            await this.gdb.sendFileExecAndSymbols(args.program);
 
             await sendTargetAttachRequest(this.gdb, { pid: args.processId });
             this.sendEvent(new OutputEvent(`attached to process ${args.processId}`));
@@ -105,19 +108,20 @@ export class GDBDebugSession extends LoggingDebugSession {
     protected async launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): Promise<void> {
         try {
             logger.setup(args.verbose ? Logger.LogLevel.Verbose : Logger.LogLevel.Warn, args.logFile || false);
+
             this.gdb.on('consoleStreamOutput', (output, category) => {
                 this.sendEvent(new OutputEvent(output, category));
             });
 
             this.gdb.on('async', (result) => this.handleGDBAsync(result));
 
-            await this.gdb.launch(args);
+            await this.gdb.spawn(args);
             await this.gdb.sendFileExecAndSymbols(args.program);
 
             this.gdb.sendEnablePrettyPrint();
 
             if (args.arguments) {
-                await exec.sendExecArguments(this.gdb, { arguments: args.arguments });
+                await sendExecArguments(this.gdb, { arguments: args.arguments });
             }
             this.sendEvent(new InitializedEvent());
             this.sendResponse(response);
@@ -191,9 +195,9 @@ export class GDBDebugSession extends LoggingDebugSession {
                                              args: DebugProtocol.ConfigurationDoneArguments): Promise<void> {
         try {
             if (this.isAttach) {
-                await exec.sendExecContinue(this.gdb);
+                await sendExecContinue(this.gdb);
             } else {
-                await exec.sendExecRun(this.gdb);
+                await sendExecRun(this.gdb);
             }
             this.isRunning = true;
             this.sendResponse(response);
@@ -266,7 +270,7 @@ export class GDBDebugSession extends LoggingDebugSession {
     protected async nextRequest(response: DebugProtocol.NextResponse,
                                 args: DebugProtocol.NextArguments): Promise<void> {
         try {
-            await exec.sendExecNext(this.gdb);
+            await sendExecNext(this.gdb);
             this.sendResponse(response);
         } catch (err) {
             this.sendErrorResponse(response, 1, err.message);
@@ -276,7 +280,7 @@ export class GDBDebugSession extends LoggingDebugSession {
     protected async stepInRequest(response: DebugProtocol.StepInResponse,
                                   args: DebugProtocol.StepInArguments): Promise<void> {
         try {
-            await exec.sendExecStep(this.gdb);
+            await sendExecStep(this.gdb);
             this.sendResponse(response);
         } catch (err) {
             this.sendErrorResponse(response, 1, err.message);
@@ -286,7 +290,7 @@ export class GDBDebugSession extends LoggingDebugSession {
     protected async stepOutRequest(response: DebugProtocol.StepOutResponse,
                                    args: DebugProtocol.StepOutArguments): Promise<void> {
         try {
-            await exec.sendExecFinish(this.gdb);
+            await sendExecFinish(this.gdb);
             this.sendResponse(response);
         } catch (err) {
             this.sendErrorResponse(response, 1, err.message);
@@ -296,7 +300,7 @@ export class GDBDebugSession extends LoggingDebugSession {
     protected async continueRequest(response: DebugProtocol.ContinueResponse,
                                     args: DebugProtocol.ContinueArguments): Promise<void> {
         try {
-            await exec.sendExecContinue(this.gdb);
+            await sendExecContinue(this.gdb);
             this.sendResponse(response);
         } catch (err) {
             this.sendErrorResponse(response, 1, err.message);
@@ -606,7 +610,7 @@ export class GDBDebugSession extends LoggingDebugSession {
         }
     }
 
-    private sendStoppedEvent(reason: string, threadId: number, exceptionText?: string) {
+    protected sendStoppedEvent(reason: string, threadId: number, exceptionText?: string) {
         // Reset frame handles and variables for new context
         this.frameHandles.reset();
         this.variableHandles.reset();
@@ -614,7 +618,7 @@ export class GDBDebugSession extends LoggingDebugSession {
         this.sendEvent(new StoppedEvent(reason, threadId, exceptionText));
     }
 
-    private handleGDBStopped(result: any) {
+    protected handleGDBStopped(result: any) {
         switch (result.reason) {
             case 'exited-normally':
                 this.sendEvent(new TerminatedEvent());
@@ -630,7 +634,7 @@ export class GDBDebugSession extends LoggingDebugSession {
         }
     }
 
-    private handleGDBAsync(result: any) {
+    protected handleGDBAsync(result: any) {
         switch (result._class) {
             case 'running':
                 // not sure we care, we do get the thread-id that's running tho

src/GDBBackend.ts
@@ -18,14 +18,6 @@ export interface MIResponse {
     _class: string;
 }
 
-export interface MIUploadRequest {
-    local: string;
-    remote?: string;
-}
-
-export interface MIUploadResponse extends MIResponse {
-}
-
 export interface MIExecNextRequest {
     reverse?: boolean;
 }
@@ -46,14 +38,7 @@ export class GDBBackend extends events.EventEmitter {
     private out?: Writable;
     private token = 0;
 
-    public launch(args: LaunchRequestArguments) {
-        const gdb = args.gdb ? args.gdb : 'gdb';
-        const proc = spawn(gdb, ['--interpreter=mi2']);
-        this.out = proc.stdin;
-        return this.parser.parse(proc.stdout);
-    }
-
-    public attach(args: AttachRequestArguments) {
+    public spawn(args: LaunchRequestArguments | AttachRequestArguments) {
         const gdb = args.gdb ? args.gdb : 'gdb';
         const proc = spawn(gdb, ['--interpreter=mi2']);
         this.out = proc.stdin;
@@ -86,14 +71,6 @@ export class GDBBackend extends events.EventEmitter {
                 reject(new Error('gdb is not running.'));
             }
         });
-    }
-
-    public sendUpload(request: MIUploadRequest): Promise<MIUploadResponse> {
-        let command = `upload ${request.local}`;
-        if (request.remote) {
-            command += ` ${request.remote}`;
-        }
-        return this.sendCommand(command);
     }
 
     public sendEnablePrettyPrint() {

commit 70a56779d45791fcfd6d2c5f76a9fe1b9c161cca

Fix attach to process (non-remote case)
The bug was that when doing an attach, an -exec-run would be done
regardless. This meant that the already attached to process
would be terminated and a new inferior would be created.

This was a regression introduced in 1893eb89150d34977ad9eaf0e806b22f3d66f0cd

To verify that the correct inferior is being communicated with
I pass a command line argument to the process to be attached.
After attaching I then verify the argv of the process has that
command line argument.

Includes improvements to the debugClient test code to allow
attachRequest to be sent as part of a connection sequence.
Includes a new evaluate method to make it a one liner to
check the value of an expression from a test.

Fixes #244

src/GDBDebugSession.ts
@@ -350,6 +350,7 @@ export class GDBDebugSession extends LoggingDebugSession {
         await this.gdb.sendEnablePrettyPrint();
 
         if (request === 'attach') {
+            this.isAttach = true;
             const attachArgs = args as AttachRequestArguments;
             await mi.sendTargetAttachRequest(this.gdb, {
                 pid: attachArgs.processId,

src/integration-tests/attach.spec.ts
@@ -1,0 +1,54 @@
+/*********************************************************************
+ * Copyright (c) 2023 Kichwa Coders and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+
+import * as cp from 'child_process';
+import * as path from 'path';
+import { AttachRequestArguments } from '../GDBDebugSession';
+import { CdtDebugClient } from './debugClient';
+import {
+    fillDefaults,
+    isRemoteTest,
+    standardBeforeEach,
+    testProgramsDir,
+} from './utils';
+import { expect } from 'chai';
+
+describe('attach', function () {
+    let dc: CdtDebugClient;
+    let inferior: cp.ChildProcess;
+    const program = path.join(testProgramsDir, 'loopforever');
+    const src = path.join(testProgramsDir, 'loopforever.c');
+
+    beforeEach(async function () {
+        dc = await standardBeforeEach();
+        inferior = cp.spawn(program, ['running-from-spawn'], {
+            cwd: testProgramsDir,
+        });
+    });
+
+    afterEach(async function () {
+        await dc.stop();
+        inferior.kill();
+    });
+
+    it('can attach and hit a breakpoint', async function () {
+        if (isRemoteTest) {
+            // attachRemote.spec.ts is the test for when isRemoteTest
+            this.skip();
+        }
+
+        const attachArgs = fillDefaults(this.test, {
+            program: program,
+            processId: `${inferior.pid}`,
+        } as AttachRequestArguments);
+        await dc.attachHitBreakpoint(attachArgs, { line: 25, path: src });
+        expect(await dc.evaluate('argv[1]')).to.contain('running-from-spawn');
+    });
+});

src/integration-tests/attachRemote.spec.ts
@@ -21,6 +21,7 @@ import {
     gdbServerPath,
     fillDefaults,
 } from './utils';
+import { expect } from 'chai';
 
 describe('attach remote', function () {
     let dc: CdtDebugClient;
@@ -31,9 +32,13 @@ describe('attach remote', function () {
 
     beforeEach(async function () {
         dc = await standardBeforeEach('debugTargetAdapter.js');
-        gdbserver = cp.spawn(gdbServerPath, [':0', emptyProgram], {
-            cwd: testProgramsDir,
-        });
+        gdbserver = cp.spawn(
+            gdbServerPath,
+            [':0', emptyProgram, 'running-from-spawn'],
+            {
+                cwd: testProgramsDir,
+            }
+        );
         port = await new Promise<number>((resolve, reject) => {
             if (gdbserver.stderr) {
                 gdbserver.stderr.on('data', (data) => {
@@ -59,18 +64,14 @@ describe('attach remote', function () {
     });
 
     it('can attach remote and hit a breakpoint', async function () {
-        await dc.hitBreakpoint(
-            fillDefaults(this.test, {
-                program: emptyProgram,
-                target: {
-                    type: 'remote',
-                    parameters: [`localhost:${port}`],
-                } as TargetAttachArguments,
-            } as TargetAttachRequestArguments),
-            {
-                path: emptySrc,
-                line: 3,
-            }
-        );
+        const attachArgs = fillDefaults(this.test, {
+            program: emptyProgram,
+            target: {
+                type: 'remote',
+                parameters: [`localhost:${port}`],
+            } as TargetAttachArguments,
+        } as TargetAttachRequestArguments);
+        await dc.attachHitBreakpoint(attachArgs, { line: 3, path: emptySrc });
+        expect(await dc.evaluate('argv[1]')).to.contain('running-from-spawn');
     });
 });

src/integration-tests/debugClient.ts
@@ -13,6 +13,7 @@ import { DebugProtocol } from '@vscode/debugprotocol';
 import * as path from 'path';
 import { defaultAdapter } from './utils';
 import * as os from 'os';
+import { expect } from 'chai';
 
 export type ReverseRequestHandler<
     A = any,
@@ -323,6 +324,52 @@ export class CdtDebugClient extends DebugClient {
         args: DebugProtocol.WriteMemoryArguments
     ): Promise<DebugProtocol.WriteMemoryResponse> {
         return this.send('writeMemory', args);
+    }
+
+    public attachHitBreakpoint(
+        attachArgs: any,
+        breakpoint: { line: number; path: string }
+    ): Promise<any> {
+        return Promise.all([
+            this.waitForEvent('initialized')
+                .then((_event) => {
+                    return this.setBreakpointsRequest({
+                        breakpoints: [{ line: breakpoint.line }],
+                        source: { path: breakpoint.path },
+                    });
+                })
+                .then((response) => {
+                    const bp = response.body.breakpoints[0];
+                    expect(bp.verified).to.be.true;
+                    expect(bp.line).to.equal(breakpoint.line);
+
+                    return Promise.all([
+                        this.configurationDoneRequest(),
+                        this.assertStoppedLocation('breakpoint', breakpoint),
+                    ]);
+                }),
+
+            this.initializeRequest().then((_response) => {
+                return this.attachRequest(attachArgs);
+            }),
+        ]);
+    }
+
+    /**
+     * Obtain the value of the expression in the context of the
+     * top frame, of the first returned thread.
+     * @param name name of the variable
+     */
+    public async evaluate(expression: string): Promise<string | undefined> {
+        const threads = await this.threadsRequest();
+        const stack = await this.stackTraceRequest({
+            threadId: threads.body.threads[0].id,
+        });
+        const evalResponse = await this.evaluateRequest({
+            expression,
+            frameId: stack.body.stackFrames[0].id,
+        });
+        return evalResponse.body.result;
     }
 }
 

src/integration-tests/test-programs/empty.c
@@ -1,4 +1,4 @@
-int main()
+int main(int argc, char *argv[])
 {
     return 0;
 }

src/integration-tests/test-programs/loopforever.c
@@ -12,7 +12,7 @@ int inner2(void) {
     return var2++;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     time_t start_time = time(NULL);
     while (stop == 0) {