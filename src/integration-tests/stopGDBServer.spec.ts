f150019939ba1cd709b43e337d588d4c92a3788c
Arsen Davtyan <Arsen.Davtyan@microchip.com>
2023-03-13 16:54:38 +0400

    1. Moved stop gdb server to separate function. 2. Added timeout after killing gdbserver. 3. Added test in integration-tests for stop gdb server.

---
+++ src/integration-tests/stopGDBServer.spec.ts
@@ -0,0 +1,49 @@
+/*********************************************************************
+ * Copyright (c) 2019 Kichwa Coders and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+
+import * as path from 'path';
+import {
+    TargetLaunchRequestArguments,
+    TargetLaunchArguments,
+} from '../GDBTargetDebugSession';
+import { CdtDebugClient } from './debugClient';
+import { fillDefaults, standardBeforeEach, testProgramsDir } from './utils';
+
+describe('stop gdbserver', function () {
+    let dc: CdtDebugClient;
+    const emptyProgram = path.join(testProgramsDir, 'empty');
+    const emptySrc = path.join(testProgramsDir, 'empty.c');
+
+    beforeEach(async function () {
+        dc = await standardBeforeEach('debugTargetAdapter.js');
+    });
+
+    afterEach(async function () {
+        const e = dc.waitForOutputEvent("server", "gdbserver stopped");
+        await dc.stop();
+        await e;
+    });
+
+    it('do something', async function () {
+        await dc.hitBreakpoint(
+            fillDefaults(this.test, {
+                program: emptyProgram,
+                target: {
+                    type: 'remote',
+                    serverParameters: [':0', emptyProgram],
+                } as TargetLaunchArguments,
+            } as TargetLaunchRequestArguments),
+            {
+                path: emptySrc,
+                line: 3,
+            }
+        );
+    });
+});
