17f67042eef237cfd2676644f000f92b2a55a1c3
Trong Le <104537989+trongle0504@users.noreply.github.com>
2023-06-02 18:19:10 +0700

    Update threadsRequest()
    Remove double-check of this.isRunning
    
    Co-authored-by: Jonah Graham <jonah@kichwacoders.com>

--- src/GDBDebugSession.ts
+++ src/GDBDebugSession.ts
@@ -861,12 +861,10 @@ export class GDBDebugSession extends LoggingDebugSession {
     ): Promise<void> {
         try {
             if (!this.isRunning) {
-                if (!this.isRunning) {
-                    const result = await mi.sendThreadInfoRequest(this.gdb, {});
-                    this.threads = result.threads.map((thread) =>
-                        this.convertThread(thread)
-                    ).sort((a, b) => a.id - b.id);
-                }
+                const result = await mi.sendThreadInfoRequest(this.gdb, {});
+                this.threads = result.threads.map((thread) =>
+                    this.convertThread(thread)
+                ).sort((a, b) => a.id - b.id);
             }
 
             response.body = {
