e832fd7b6bc927707836a2eb03714be9fad1756f
Omar Elkhouly <omar.elkhouly@arm.com>
2025-05-12 14:40:13 +0200

    suppressing -var-create error

--- src/gdb/GDBDebugSessionBase.ts
+++ src/gdb/GDBDebugSessionBase.ts
@@ -1272,10 +1272,8 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
 
             this.sendResponse(response);
         } catch (err) {
-            if (err instanceof Error) {
-                if (err.message === '-var-create: unable to create variable object') {
-                    this.sendResponse(response);
-                }
+            if (err instanceof Error && err.message === '-var-create: unable to create variable object') {
+                this.sendResponse(response);
             } else {
                 this.sendErrorResponse(
                     response,
