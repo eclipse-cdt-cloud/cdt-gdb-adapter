d7e8c00c40cea5512ca5fdfe6ce627298fb4030c
Jens Reinecke <jens.reinecke@arm.com>
2025-06-20 13:24:05 +0200

    prettier
    Signed-off-by: Jens Reinecke <jens.reinecke@arm.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,9 @@
 
 ## 1.0.10
 
--   Refactor [#362](https://github.com/eclipse-cdt-cloud/cdt-gdb-adapter/issues/362): Cannot execute CLI commands like > interrupt from Debug Console while CPU is running
--   Feature Request [#385](https://github.com/eclipse-cdt-cloud/cdt-gdb-adapter/pull/385): Setting a warning for enable/disable breakpoint commands
+- Refactor [#362](https://github.com/eclipse-cdt-cloud/cdt-gdb-adapter/issues/362): Cannot execute CLI commands like > interrupt from Debug Console while CPU is running
+- Feature Request [#385](https://github.com/eclipse-cdt-cloud/cdt-gdb-adapter/pull/385): Setting a warning for enable/disable breakpoint commands
 
 ## 1.0.9
 
--   Implements [#360](https://github.com/eclipse-cdt-cloud/cdt-gdb-adapter/issues/360): Support GDB/MI breakpoint notifications.
+- Implements [#360](https://github.com/eclipse-cdt-cloud/cdt-gdb-adapter/issues/360): Support GDB/MI breakpoint notifications.
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,33 +6,33 @@ import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 const jsLanguageOptions = {
-    globals: {
-      // Add any specific globals you need
-      ...globals.node,
-    },
+  globals: {
+    // Add any specific globals you need
+    ...globals.node,
+  },
 };
 
 const tsLanguageOptions = {
-    parser: tseslint.parser,
-    parserOptions: {
-      project: './tsconfig.json'
-    },
-    ecmaVersion: 2015,
-    sourceType: 'module',
-    globals: {
-      // Add any specific globals you need
-      ...globals.node,
-    },
+  parser: tseslint.parser,
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  ecmaVersion: 2015,
+  sourceType: 'module',
+  globals: {
+    // Add any specific globals you need
+    ...globals.node,
+  },
 };
 
 export default [
   {
     ignores: [
-        // don't ever lint node_modules
-        '**/node_modules/**',
-        // don't lint build output (make sure
-        // it's set to your correct build folder name)
-        'dist/**'
+      // don't ever lint node_modules
+      '**/node_modules/**',
+      // don't lint build output (make sure
+      // it's set to your correct build folder name)
+      'dist/**',
     ],
   },
   {
@@ -43,8 +43,8 @@ export default [
   },
   // Recommended rules config array must only apply
   // to Typescript files, not plain Javascript, and
-  // should use same language options as 
-  ...tseslint.configs.recommended.map(config => ({
+  // should use same language options as
+  ...tseslint.configs.recommended.map((config) => ({
     files: ['**/*.ts', '**/*.tsx'],
     ...config,
     languageOptions: tsLanguageOptions,
@@ -58,25 +58,25 @@ export default [
     },
     rules: {
       ...prettier.rules,
-      "@typescript-eslint/no-explicit-any": "off",
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
         {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
         },
       ],
       // For backwards compatibility with older recommended rules
       // in eslint/typescript-eslint, based on existing "disable"
       // directives
-      "@typescript-eslint/no-non-null-assertion": "error",
+      '@typescript-eslint/no-non-null-assertion': 'error',
       // Turn off the "no-unused-expressions" default rules...
-      "no-unused-expressions": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
+      'no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
       // ... and turn on the chai-friendly version
-      "chai-friendly/no-unused-expressions": "error",
+      'chai-friendly/no-unused-expressions': 'error',
     },
   },
 ];
