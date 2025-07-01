da0a2e356cadb9dad09b5ad6511ae1adf7e55368
Jens Reinecke <jens.reinecke@arm.com>
2025-07-01 09:54:51 +0200

    Cancel pending GDB pipe writes and MI parser command queue entries on pipe close event. Unblocks potentially stalled communication.
    Signed-off-by: Jens Reinecke <jens.reinecke@arm.com>

--- src/MIParser.ts
+++ src/MIParser.ts
@@ -25,6 +25,17 @@ export class MIParser {
 
     constructor(protected gdb: IGDBBackend) {}
 
+    public cancelQueue() {
+        // Entries in the form of [key, callback]
+        const entries = Object.entries(this.commandQueue);
+        entries.forEach((entry) => {
+            // Call callback with error to reject command promise
+            entry[1]('error', { msg: 'MI parser command queue cancelled' });
+            // Delete command by key
+            delete this.commandQueue[entry[0]];
+        });
+    }
+
     public parse(stream: Readable): Promise<void> {
         return new Promise((resolve) => {
             this.waitReady = resolve;
