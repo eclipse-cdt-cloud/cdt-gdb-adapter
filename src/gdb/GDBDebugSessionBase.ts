34cb33f1a3b07b95c1cadcd07f5aecd6d1ae40a6
Jens Reinecke <jens.reinecke@arm.com>
2025-07-30 11:27:43 +0200

    p r e t t i e r
    Signed-off-by: Jens Reinecke <jens.reinecke@arm.com>

--- src/gdb/GDBDebugSessionBase.ts
+++ src/gdb/GDBDebugSessionBase.ts
@@ -2139,8 +2139,7 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
                         );
                     }
                     updateThreads.forEach(
-                        (thread) =>
-                            (thread.lastRunToken = token)
+                        (thread) => (thread.lastRunToken = token)
                     );
                 }
                 break;
