8d9715c687c9c25b63c66fe245db660a87a95fb5
Omar Elkhouly <152891311+omarArm@users.noreply.github.com>
2025-08-08 16:15:26 +0200

    Disabling evaluate request error messages when hovering over comments (#420)
    * changing lookup for var-create error message to use includes instead of exact replica

--- src/gdb/GDBDebugSessionBase.ts
+++ src/gdb/GDBDebugSessionBase.ts
@@ -1539,10 +1539,7 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
 
             this.sendResponse(response);
         } catch (err) {
-            if (
-                err instanceof Error &&
-                err.message === '-var-create: unable to create variable object'
-            ) {
+            if (err instanceof Error && err.message.includes('var-create')) {
                 if (args.context === 'hover') {
                     response.success = false;
                 }
