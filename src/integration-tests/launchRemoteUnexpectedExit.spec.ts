d0ef2b1fb0fb2aaf76b93c889a770f8b62cbbcd4
Jens Reinecke <jens.reinecke@arm.com>
2026-04-14 08:31:09 +0200

    Unify to Arm Limited in copyright headers (#524)
    Signed-off-by: Jens Reinecke <jens.reinecke@arm.com>

--- src/integration-tests/launchRemoteUnexpectedExit.spec.ts
+++ src/integration-tests/launchRemoteUnexpectedExit.spec.ts
@@ -1,5 +1,5 @@
 /*********************************************************************
- * Copyright (c) 2025 Arm Ltd., Kichwa Coders and others
+ * Copyright (c) 2025 Arm Limited, Kichwa Coders and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
