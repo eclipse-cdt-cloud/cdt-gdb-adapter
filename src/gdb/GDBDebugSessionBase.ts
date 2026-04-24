7dacc9c1bb77985c00e7c989449d7648a0aab94a
Christian Walther <walther@indel.ch>
2026-04-24 15:15:32 +0200

    Defer continued/stopped events to the end of the configuration phase (#548 2/7)
    When attaching, the initial `running` notifications from GDB about existing threads are suppressed from going to the client as `continued` events, these are unnecessary because running is the default state of a thread, and would result in a barrage of `threads` requests from the client. Previously, this suppression was upheld until the beginning of the configuration phase, marked by the `initialized` event. In some scenarios involving GDB’s `extended-remote` target, this is too early. Extend the suppression until the end of the configuration phase, shortly before the `configurationDone` response.
    
    This leaves the `isInitialized` variable unused, but since it is `protected` and may already be used by third-party subclasses, I am leaving it in.
    
    Moving the setting of configuringState = DONE after sendExecContinue is needed to fix the test failures from the previous commit, because it allows the `continued` event caused by this to cancel the problematic `stopped` event in deferredStopEvents. This does not disturb the working of the configuringState state machine.

--- src/gdb/GDBDebugSessionBase.ts
+++ src/gdb/GDBDebugSessionBase.ts
@@ -212,7 +212,7 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
     // keeps track of where in the configuration phase (between initialize event
     // and configurationDone response) we are
     protected configuringState: ConfiguringState = ConfiguringState.INITIAL;
-    protected isInitialized = false;
+    protected isInitialized = false; // unused here but kept for compatibility
     protected deferredStopEvents: any[] = [];
 
     /**
@@ -609,10 +609,6 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
         }
         this.sendEvent(new InitializedEvent());
         this.isInitialized = true;
-        for (const resultData of this.deferredStopEvents) {
-            this.handleGDBStopped(resultData);
-        }
-        this.deferredStopEvents.splice(0);
     }
 
     protected async attachRequest(
@@ -778,12 +774,19 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
         this.waitPaused = undefined;
     }
 
+    protected setConfiguringStateDone(): void {
+        this.configuringState = ConfiguringState.DONE;
+        for (const resultData of this.deferredStopEvents) {
+            this.handleGDBStopped(resultData);
+        }
+        this.deferredStopEvents.splice(0);
+    }
+
     protected async continueIfNeeded(): Promise<void> {
         if (this.pauseCount > 0) {
             this.pauseCount--;
             if (this.pauseCount === 0) {
                 if (this.configuringState === ConfiguringState.FINISHING) {
-                    this.configuringState = ConfiguringState.DONE;
                     if (
                         this.runAfterConfiguration == RequestArgRun.ALL ||
                         this.waitPausedNeeded
@@ -794,6 +797,7 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
                             await mi.sendExecRun(this.gdb);
                         }
                     }
+                    this.setConfiguringStateDone();
                 } else if (this.waitPausedNeeded) {
                     if (this.gdb.isNonStopMode()) {
                         await mi.sendExecContinue(
@@ -1731,7 +1735,7 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
                 this.configuringState = ConfiguringState.FINISHING;
                 await this.continueIfNeeded();
             } else {
-                this.configuringState = ConfiguringState.DONE;
+                this.setConfiguringStateDone();
             }
             this.sendResponse(response);
         } catch (err) {
@@ -3209,7 +3213,7 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
                     }
                 }
                 this.updateIsRunning();
-                if (this.isInitialized) {
+                if (this.configuringState == ConfiguringState.DONE) {
                     this.handleGDBResume(resultData);
                 } else {
                     // while we are deferring events, cancel previous stop events for the same thread
@@ -3288,7 +3292,7 @@ export abstract class GDBDebugSessionBase extends LoggingDebugSession {
                         newThreadsConfirmed ||
                         (wasRunning && !this.isRunning))
                 ) {
-                    if (this.isInitialized) {
+                    if (this.configuringState == ConfiguringState.DONE) {
                         this.handleGDBStopped(resultData);
                     } else {
                         this.deferredStopEvents.push(resultData);
